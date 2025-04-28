cbac6c76d18532fca400b6e241b8a0ae86a466ef
pdromarks <pdro0214@gmail.com>
2025-04-28 13:07:13 -0300

    Aula 2 - Aprendento mais sobre os elementos: beforeEach() => {}; cy.get(); .type(); .click(); .should()

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,9 +1,26 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  
-  it ('Verifica o título da aplicação',() => {
-    cy.visit('./src/index.html')
+  beforeEach(()=> { //executa antes de todos os testes
 
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    cy.visit('./src/index.html') 
+    //visita a página local    
+  })
+
+  it ('Verifica o título da aplicação',() => {
+
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') 
+    // verifica se o tittle é igual...
+  })
+
+  it.only ('Preenche os campos obrigatórios e envia o formulário',() => {
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Marques')
+    cy.get('#email').type('pedro@gmail.com') 
+    cy.get('#open-text-area').type('Obrigado')
+    //o cy.get pega um elemento html pelo class '.' ou pelo id '#', além disso o .type digita no campo informado no get
+    cy.get('button[type="submit"]').click()
+    // o .click clica no item indicado no get
+    cy.get('.success').should('be.visible')
+    // o .should valida a condição que está em (''), que nesse caso é se está visível
   })
 
 })
